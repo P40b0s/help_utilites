c88e9cb1d85e6090d0482c9f02db303f511a0a13
P40b0s <uranius666@gmail.com>
2024-04-25 14:38:43 +0300

    добавил реекспорт io

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #[cfg(feature="dates")]
 mod dates;
 mod io;
+pub use io::read_file_to_binary;
 mod serialize;
 pub use serialize::{serialize_to_file, empty_string_as_none, null_string_as_none};
 #[cfg(feature="dates")]
